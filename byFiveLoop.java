656ab98453a330670fdcf811007a936ab768565b
Sam Lijo <samuelvillalvalijo@gmail.com>
2021-08-11 16:59:15 -0500

    playing around with for loop

---
+++ byFiveLoop.java
@@ -0,0 +1,19 @@
+// Lord Alaya Vijana
+// Program to loop by increments of 5 to test if I can do it or not
+
+public class byFiveLoop{
+
+
+	public static void main( String []args) {
+		int sum = 0;
+	
+		//create the for loop that loops by five
+
+		for ( int i = 0;i< 100; i +=5){
+			sum = i;
+			System.out.println("The current Sum is "+sum);
+		}
+	}
+}
+
+
